d363564ba1ba67ca97b34adb959348e27f8fbc36
robertoalyssom <robert_alys@hotmail.com>
2025-09-09 11:32:14 -0300

    fix(cors): remove trailing slash from Vercel origin

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://user-registration-eight-liard.vercel.app/"],
+    origin: "https://user-registration-eight-liard.vercel.app", // allow requests from this origin
     credentials: true, // allow cookies to be sent with the request
   })
 );
